4540e4bdeb1260f9ae5a678b9a2e79c0d81af0fa
Jose Blanco <joseblanco77@gmail.com>
2017-07-07 15:29:25 -0600

    Adding sql file for test in the server

---
+++ sql/pqwdsl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `pqwsdl` (
+  `id_wsdl` int(11) unsigned NOT NULL auto_increment,
+  `id_stock` int(11) unsigned NOT NULL default 0,
+  `response` text NOT NULL default '',
+  `status` varchar(5) NOT NULL default '',
+  `params` text NOT NULL default '',
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id_wsdl`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
